083742698b6d93772e8e8d903db188525e473ba4
Ahmed Khalil Boulahia <ahmedkhalilboulahia@gmail.com>
2021-07-14 13:09:12 +0100

    Create 14-saddle_up.py

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" contains a function that multiplies two matrices"""
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """ matrix multiplication"""
+    return (np.dot(mat1, mat2))
